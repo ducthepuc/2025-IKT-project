5f6a0cb5efef5392f221c11943366ab611a60894
ducthepuc <ducthepuc@outlook.com>
2025-01-27 09:09:43 +0100

    Got started, got cooked by WEBSTORM TERMINAL :(:(:(:(:(

---
+++ ClientApp/tailwind/tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+    content: [
+        '../index.html',
+        '../src/**/*.{vue,js,ts,jsx,tsx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#1DA1F2',
+                secondary: '#FF9900',
+            },
+        },
+    },
+    plugins: [],
+};

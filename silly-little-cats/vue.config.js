8b4e456609f470f6dc6d4960d3c6a7d637cb78a5
wiwiwi <bence.holes@gmail.com>
2025-06-01 16:40:45 +0200

    docker?

--- silly-little-cats/vue.config.js
+++ silly-little-cats/vue.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '^api': {
-        target: 'http://localhost:3000',
+        target: 'http://backend:3000',
         changeOrigin: true,
       }
     }
